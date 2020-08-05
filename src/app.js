97b70f29400f283d8370e5ec27bb0d00a00b4b3a
Roger Rodriguez <rrod@hey.com>
2020-08-05 18:34:36 -0400

    Adding scaffolding

---
+++ src/app.js
@@ -0,0 +1,6 @@
+/**
+ * We'll use app.js to build our view from modules
+ * in main.js
+ */
+
+import { convertDate } from "./main";
---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ * Main.js contains our project modules
+ * Write your functions below
+ */
+
+/**
+ *
+ * @param {number} time - Unix time in seconds returns it in miliseconds
+ */
+export function convertDate(time) {
+  return time * 1000;
+}
